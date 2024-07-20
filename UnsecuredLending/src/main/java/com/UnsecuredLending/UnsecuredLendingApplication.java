b48cc7495f8afa9b1949824d091d9166e80b086a
palljosh <pallavi.a.joshi@capgemini.com>
2024-07-20 23:59:14 +0530

    Initial commit

---
+++ UnsecuredLending/src/main/java/com/UnsecuredLending/UnsecuredLendingApplication.java
@@ -0,0 +1,13 @@
+package com.UnsecuredLending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsecuredLendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsecuredLendingApplication.class, args);
+	}
+
+}
---
+++ UnsecuredLending/src/test/java/com/UnsecuredLending/UnsecuredLendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UnsecuredLending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsecuredLendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
